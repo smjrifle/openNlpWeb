55ea86573d544138e45157f9da253e3a8627926f
Paras Nath Chaudhary <opnchaudhary@gmail.com>
2012-08-17 10:58:13 +0545

    testing wordnet

--- src/test/myTest.java
+++ src/test/myTest.java
@@ -1,17 +1,56 @@
 package test;
 
+import java.util.Scanner;
+
+import edu.smu.tspell.wordnet.NounSynset;
+import edu.smu.tspell.wordnet.Synset;
+import edu.smu.tspell.wordnet.SynsetType;
+import edu.smu.tspell.wordnet.VerbSynset;
+import edu.smu.tspell.wordnet.WordNetDatabase;
+
 public class myTest {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String[] temp=null;
-		for(int i=0;i<5;i++){
+		
+		System.out.println("It starts here...");
+		//set the directory for the  wordnet database
+		String wnetDir="/usr/share/wordnet-3.0/dict";
+		System.setProperty("wordnet.database.dir", wnetDir);
+		
+		NounSynset nounSynset;
+		VerbSynset verbSynset;
+		NounSynset[] hyponyms;
+		VerbSynset[] hypernyms;
+		
+		WordNetDatabase database=WordNetDatabase.getFileInstance();
+		
+		Scanner s=new Scanner(System.in);
+		while(true){
+			System.out.println("Enter a word: ");
+			String wordToSearch=s.next();
 			
+			
+			Synset[] synsets = database.getSynsets(wordToSearch, SynsetType.NOUN);
+			for (int i = 0; i < synsets.length; i++) {
+				nounSynset = (NounSynset)(synsets[i]);
+				hyponyms = nounSynset.getHyponyms();
+				System.err.println(nounSynset.getWordForms()[0] +
+						": " + nounSynset.getDefinition() + ") has " + hyponyms.length + " hyponyms");
+			}
+			
+			
+			synsets=database.getSynsets(wordToSearch, SynsetType.VERB);
+			for (int i = 0; i < synsets.length; i++) {
+				verbSynset = (VerbSynset)(synsets[i]);
+				hypernyms=verbSynset.getHypernyms();
+				System.err.println(verbSynset.getWordForms()[0] +
+						": " + verbSynset.getDefinition() + ") has " + hypernyms.length + " hypernyms");
+			}
 		}
-		System.out.println(temp[1]);
+		
 	}
 
 }
